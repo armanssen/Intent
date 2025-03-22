cd9bb7b076dfc53ddd8f03aa3f75b29b29e9e525
armanseisenbek <arman.seisenbek@gmail.com>
2025-03-23 00:39:13 +0500

    master: add initial bottom sheet

--- app/src/main/java/com/crux/screens/main/ui/MainScreen.kt
+++ app/src/main/java/com/crux/screens/main/ui/MainScreen.kt
@@ -1,24 +1,37 @@
 package com.crux.screens.main.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -40,6 +53,11 @@ internal fun MainScreen(
     onEvent: (MainScreenEvent) -> Unit = viewModel::onEvent
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState()
+
     Scaffold(
         modifier = modifier
             .nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -48,7 +66,15 @@ internal fun MainScreen(
             TopAppBar(
                 scrollBehavior = scrollBehavior,
                 title = {
-
+                    Text(
+                        text = "Default",
+                        modifier = Modifier
+                            .clickable(
+                                onClick = {
+                                    openBottomSheet = !openBottomSheet
+                                }
+                            )
+                    )
                 },
                 navigationIcon = {
                     IconButton(
@@ -90,25 +116,60 @@ internal fun MainScreen(
                 contentPadding = PaddingValues(
                     vertical = 8.dp,
                     horizontal = 12.dp
-                )
-            ) {
-                items(uiState.tasks) { task ->
-                    TaskListItemView(
-                        task = task,
-                        onClick = {
-                            onClickTask(task.id)
-                        },
-                        onCheckedChange = { isChecked ->
-                            onEvent(
-                                MainScreenEvent.OnCheckedChange(
-                                    task = task,
-                                    isChecked = isChecked
+                ),
+                content = {
+                    items(uiState.tasks) { task ->
+                        TaskListItemView(
+                            task = task,
+                            onClick = {
+                                onClickTask(task.id)
+                            },
+                            onCheckedChange = { isChecked ->
+                                onEvent(
+                                    MainScreenEvent.OnCheckedChange(
+                                        task = task,
+                                        isChecked = isChecked
+                                    )
                                 )
-                            )
-                        }
-                    )
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    }
+
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            modifier = Modifier.statusBarsPadding(),
+            shape = RoundedCornerShape(
+                topStart = 4.dp,
+                topEnd = 4.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 0.dp
+            ),
+            dragHandle = null,
+            onDismissRequest = { openBottomSheet = false },
+            sheetState = bottomSheetState,
+            content = {
+                LazyColumn {
+                    items(25) {
+                        ListItem(
+                            headlineContent = { Text("Item $it") },
+                            leadingContent = {
+                                Icon(
+                                    Icons.Default.Favorite,
+                                    contentDescription = "Localized description"
+                                )
+                            },
+                            colors =
+                                ListItemDefaults.colors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceContainerLow
+                                ),
+                        )
+                    }
                 }
             }
-        }
+        )
     }
 }
