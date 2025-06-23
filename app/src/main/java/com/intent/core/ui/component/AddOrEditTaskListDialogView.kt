dbc89cab745eed4e7fb8757e4f93bbe8163373c3
armanseisenbek <arman.seisenbek@gmail.com>
2025-06-23 15:58:15 +0500

    master: [fix] fix AddOrEditTaskListDialogView ui bugs

--- app/src/main/java/com/intent/core/ui/component/AddOrEditTaskListDialogView.kt
+++ app/src/main/java/com/intent/core/ui/component/AddOrEditTaskListDialogView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
@@ -17,7 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
@@ -42,14 +46,27 @@ fun AddOrEditTaskListDialogView(
 ) {
     val focusRequester = remember { FocusRequester() }
 
+    var textFieldValueState by remember {
+        mutableStateOf(
+            TextFieldValue(
+                text = textFieldValue,
+                selection = TextRange(
+                    index = textFieldValue.length
+                )
+            )
+        )
+    }
+
     LaunchedEffect(Unit) {
         focusRequester.requestFocusWithDelay()
     }
+
     Dialog(
         onDismissRequest = onDismissRequest
     ) {
         Column(
             modifier = Modifier
+                .imePadding()
                 .clip(MaterialTheme.shapes.medium)
                 .background(MaterialTheme.colorScheme.surfaceContainer)
                 .padding(16.dp)
@@ -75,11 +92,9 @@ fun AddOrEditTaskListDialogView(
                     .focusRequester(focusRequester)
                     .padding(vertical = 16.dp)
                     .fillMaxWidth(),
-                value = TextFieldValue(
-                    text = textFieldValue,
-                    selection = TextRange(textFieldValue.length)
-                ),
+                value = textFieldValueState,
                 onValueChange = {
+                    textFieldValueState = it
                     onValueChange(it.text)
                 },
                 keyboardOptions = KeyboardOptions.Default.copy(
