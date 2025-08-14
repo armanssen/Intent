9a18b0dbe5199af21786fa184c378bc986f1ca52
armanseisenbek <arman.seisenbek@gmail.com>
2025-08-14 09:13:14 +0500

    master: [feat] add initial delete data button to settings

--- app/src/main/java/com/intent/screens/settings/ui/SetttingsScreen.kt
+++ app/src/main/java/com/intent/screens/settings/ui/SetttingsScreen.kt
@@ -1,9 +1,18 @@
 package com.intent.screens.settings.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,7 +23,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.intent.core.ui.theme.IntentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,7 +64,43 @@ internal fun SettingsScreen(
         }
     ) { padding ->
         Column(modifier = Modifier.padding(padding)) {
-
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .clickable(onClick = {})
+                    .clip(MaterialTheme.shapes.small)
+                    .background(Color(0xFFFFCDD2))
+                    .padding(12.dp)
+                    .fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = "delete icon",
+                    tint = Color(0xFFF44336)
+                )
+                Spacer(Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = "Delete all data",
+                        color = Color(0xFFF44336),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Spacer(Modifier.height(4.dp))
+                    Text(
+                        text = "This will remove all settings and data stored in the app.",
+                        color = Color(0xFFF44336),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    SettingsScreen(
+        navigateBack = {}
+    )
 }
